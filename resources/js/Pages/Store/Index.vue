7462b6f8f43a68cedd150d4a5394aad7932848c3
Milan <milan90pantovic@gmail.com>
2023-08-01 16:08:38 +0200

    Started seeding
    Started adding seeders

--- resources/js/Pages/Store/Index.vue
+++ resources/js/Pages/Store/Index.vue
@@ -26,6 +26,56 @@ const deleteStore = (store) => {
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Prodavnice
+
+                <svg
+                    height="40px"
+                    width="40px"
+                    version="1.1"
+                    id="Layer_1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 496 496"
+                    xml:space="preserve"
+                >
+                    <path
+                        style="fill: #b3dce0"
+                        d="M496,309.6c0,27.2-6.4,48.8-32.8,48.8H32.8C5.6,358.4,0,336.8,0,309.6V186.4
+	c0-27.2,21.6-48.8,48.8-48.8h398.4c27.2,0,48.8,21.6,48.8,48.8L496,309.6L496,309.6z"
+                    />
+                    <g>
+                        <path
+                            style="fill: #c1e3e5"
+                            d="M0,186.4c0-27.2,21.6-48.8,48.8-48.8h398.4c27.2,0,48.8,21.6,48.8,48.8"
+                        />
+                    </g>
+                    <path
+                        style="fill: #9ecacc"
+                        d="M464.8,308.8c0,5.6-4,9.6-9.6,9.6H40.8c-5.6,0-9.6-4-9.6-9.6l0,0c0-5.6,4-9.6,9.6-9.6h414.4
+	C460.8,299.2,464.8,303.2,464.8,308.8L464.8,308.8z"
+                    />
+                    <path
+                        style="fill: #7eaaaa"
+                        d="M31.2,308.8L31.2,308.8c0-5.6,4-9.6,9.6-9.6h414.4c5.6,0,9.6,4,9.6,9.6l0,0"
+                    />
+                    <path
+                        style="fill: #9ecacc"
+                        d="M464.8,336c0,5.6-4,9.6-9.6,9.6H40.8c-5.6,0-9.6-4-9.6-9.6l0,0c0-5.6,4-9.6,9.6-9.6h414.4
+	C460.8,326.4,464.8,330.4,464.8,336L464.8,336z"
+                    />
+                    <path
+                        style="fill: #7eaaaa"
+                        d="M31.2,336L31.2,336c0-5.6,4-9.6,9.6-9.6h414.4c5.6,0,9.6,4,9.6,9.6l0,0"
+                    />
+                    <path
+                        style="fill: #46bc5c"
+                        d="M145.6,267.2c0,2.4-2.4,4.8-4.8,4.8H39.2c-2.4,0-4.8-2.4-4.8-4.8v-33.6c0-2.4,2.4-4.8,4.8-4.8h102.4
+	c2.4,0,4.8,2.4,4.8,4.8v33.6H145.6z"
+                    />
+                    <path
+                        style="fill: #2fa53f"
+                        d="M140.8,229.6c2.4,0,4.8,2.4,4.8,4.8V268c0,2.4-2.4,4.8-4.8,4.8H39.2c-2.4,0-4.8-2.4-4.8-4.8"
+                    />
+                </svg>
             </h2>
         </template>
 
@@ -109,33 +159,45 @@ const deleteStore = (store) => {
                                                 <td
                                                     class="py-2 px-4 border-b text-center"
                                                 >
-                                                    <Link
-                                                        :href="`${route(
-                                                            'prodavnice.edit',
-                                                            store
-                                                        )}`"
-                                                        class="inline-block text-sm mt-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                                                    <div
+                                                        class="flex items-baseline"
                                                     >
-                                                        {{ __("helpers.edit") }}
-                                                    </Link>
+                                                        <Link
+                                                            :href="`${route(
+                                                                'prodavnice.edit',
+                                                                store
+                                                            )}`"
+                                                            class="inline-block text-sm mt-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                                                        >
+                                                            {{
+                                                                __(
+                                                                    "helpers.edit"
+                                                                )
+                                                            }}
+                                                        </Link>
 
-                                                    <DangerButton
-                                                        class="ml-3"
-                                                        @click="
-                                                            deleteStore(store)
-                                                        "
-                                                        :class="{
-                                                            'opacity-25':
-                                                                store.processing,
-                                                        }"
-                                                        :disabled="
-                                                            store.processing
-                                                        "
-                                                    >
-                                                        {{
-                                                            __("helpers.delete")
-                                                        }}
-                                                    </DangerButton>
+                                                        <DangerButton
+                                                            class="ml-3"
+                                                            @click="
+                                                                deleteStore(
+                                                                    store
+                                                                )
+                                                            "
+                                                            :class="{
+                                                                'opacity-25':
+                                                                    store.processing,
+                                                            }"
+                                                            :disabled="
+                                                                store.processing
+                                                            "
+                                                        >
+                                                            {{
+                                                                __(
+                                                                    "helpers.delete"
+                                                                )
+                                                            }}
+                                                        </DangerButton>
+                                                    </div>
                                                 </td>
                                             </tr>
                                         </template>
